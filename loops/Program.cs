9e12b52ecd9320d121c2db78f41295c490164f88
Shalem Raj Bogiri <Bogiri-SR@ulster.ac.uk>
2024-10-01 10:41:39 +0100

    month

--- loops/Program.cs
+++ loops/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq.Expressions;
+
+BuildSwitchStatement();
+void BuildSwitchStatement()
+
+{
+    Console.WriteLine("Enter a month (1-3): ");
+    int month= Convert.ToInt32(Console.ReadLine());
+    string monthname;
+
+    switch(month)
+
+    {
+        case 1:
+            monthname = "January";
+            break;
+
+        case 2:
+
+            monthname = "Feb";
+            break;
+
+        case 3:
+
+            monthname = "March";
+            break;
+
+        default:
+
+            monthname = " Invalid month";
+            break;
+
+    }
+
+    Console.WriteLine($" month {month} is {monthname} ");
+
+   
+}
+
+
+
+
